14ad4d3e8571b84338ce8e59d8ea6541d8efa5ce
vnteam247-hub <vnteam247@gmail.com>
2026-07-23 21:05:10 +0700

    Create lesson1.js

---
+++ data/lesson1.js
@@ -0,0 +1,18 @@
+JSON
+{
+    "subTypeTitle": "CHỦ ĐỀ: GIA ĐÌNH & CON VẬT (50 CÂU - 30S/CÂU)",
+    "title": "UNIT 2: FAMILY & PETS COLLOCATIONS",
+    "items": [
+        { 
+            "type": "phrase", 
+            "text": "A close-knit family", 
+            "ipa": "/ə kləʊs nɪt ˈfæməli/", 
+            "meaning": "Một gia đình gắn bó khăng khít" 
+        },
+        { 
+            "type": "sentence", 
+            "text": "My parents always encourage me to study hard", 
+            "meaning": "Bố mẹ tôi luôn khuyến khích tôi học tập chăm chỉ" 
+        }
+    ]
+}
